0d9502fc36fd6295f7b8dfc43afee61578e1ee31
Salil999 <ssaxen4@illinois.edu>
2015-04-11 17:45:12 -0500

    update
    the thing that didn’t work now sort of works, will make it work more
    when it works more

--- led_update.py
+++ led_update.py
@@ -6,10 +6,10 @@ import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 
-GPIO.setup(4, GPIO.OUT) # pin4 = Green LED
+GPIO.setup(4, GPIO.OUT) #pin4 = Green LED
 GPIO.setup(17, GPIO.OUT) #pin17 = Red LED
 
-r = requests.get('https://desolate-stream-1847.herokuapp.com/read/0')
+r = requests.get('https://fathomless-wave-8947.herokuapp.com/read/0')
 #print(r.url)
 
 data = r.text
@@ -19,13 +19,16 @@ data_arr = json.loads(data)
 print(data)
 print(data_arr)
 
-if data_arr["vacant"] == True:
-	#print("true!")
-	GPIO.output(4, 1)
-	GPIO.output(17, 0)
-else:
-	#print("false!")
-	GPIO.output(4, 0)
-	GPIO.output(17, 1)
+while 1==1: #This will pull from the server forever!
+	if data_arr["vacant"] == True:
+		print("True!")
+		time.sleep(2)
+		GPIO.output(4, 1)
+		GPIO.output(17, 0)
+	else:
+		print("False!")
+		time.sleep(2)
+		GPIO.output(4, 0)
+		GPIO.output(17, 1)
 
 GPIO.cleanup()
