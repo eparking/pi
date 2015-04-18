13b1d0598bdc3f2cc16e129e4d5d04202cb58741
jblum963 <jablum2@illinois.edu>
2015-04-18 14:23:31 -0500

    Update led_update.py
    Fixed the bug by moving the requests call into the while loop so that the pi's updating its information from the server rather than using the same old information over and over.

--- led_update.py
+++ led_update.py
@@ -9,17 +9,17 @@ GPIO.setwarnings(False)
 GPIO.setup(4, GPIO.OUT) #pin4 = Green LED
 GPIO.setup(17, GPIO.OUT) #pin17 = Red LED
 
-r = requests.get('https://fathomless-wave-8947.herokuapp.com/read/0')
-#print(r.url)
-
-data = r.text
-
-data_arr = json.loads(data)
-
-print(data)
-print(data_arr)
-
 while 1==1: #This will pull from the server forever!
+	r = requests.get('https://fathomless-wave-8947.herokuapp.com/read/0')
+	#print(r.url)
+
+	data = r.text
+
+	data_arr = json.loads(data)
+
+	print(data)
+	print(data_arr)
+	
 	if data_arr["vacant"] == True:
 		print("True!")
 		time.sleep(2)
