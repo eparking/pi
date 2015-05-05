eaf40f29da854ee5b4a43a5d1a5a8fecc919d55d
Salil999 <ssaxen4@illinois.edu>
2015-05-04 19:00:21 -0500

    edited and cleaned up code

--- led_update.py
+++ led_update.py
@@ -3,10 +3,11 @@ import json
 import RPi.GPIO as GPIO
 import time
 import math
+import urllib3
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(3, GPIO.OUT) #pin3 = Green LED
+GPIO.setup(27, GPIO.OUT) #pin27 = Green LED
 GPIO.setup(4, GPIO.OUT) #pin4 = Red LED
 GPIO.setup(17, GPIO.OUT) #pin17 = Blue LED
 
@@ -20,8 +21,10 @@ temp = 0
 
 while True: #This will pull from the server forever!
 	# if total_time < end:
-		r = requests.get('https://fathomless-wave-8947.herokuapp.com/read/0')
+		urllib3.disable_warnings()
+		r = requests.get('http://eparking196.herokuapp.com/read/1')
 		if r.status_code == 200: # this checks for internet connection
+			urllib3.disable_warnings()
 			#print(r.url)
 			data = r.text
 			data_arr = json.loads(data)
@@ -38,36 +41,40 @@ while True: #This will pull from the server forever!
 			print(str(math.ceil(hours)) + ' hour(s)')
 			print(str(math.ceil(mins)) + ' minute(s)')
 			print(str(math.ceil(seconds)) + ' second(s)')
-			if str(data_arr["vacant"]) == "True":
+			if data_arr["vacant"] == 0:
 				if x == True:
 					start_time = end
 					x = False
 				#this is if the spot is available (GREEN LIGHT)
-				#using pin 3 to correspond to available
+				#using pin 27 to correspond to available
 				print("true!")
-				GPIO.output(3, 1) # green light is on
+				urllib3.disable_warnings()
+				GPIO.output(27, 1) # green light is on
 				GPIO.output(4, 0) # red light is off
 				GPIO.output(17,0) # blue light is off
-			elif str(data_arr["vacant"]) == "False":
+			elif data_arr["vacant"] == 1:
 				end = time.time()
 				#this is if the spot is occupied (RED LIGHT)
 				#using pin 4 to correspond to occupied
 				print("false!")
-				GPIO.output(3, 0) # green light is off
+				urllib3.disable_warnings()
+				GPIO.output(27, 0) # green light is off
 				GPIO.output(4, 1) # red light is on
 				GPIO.output(17,0) # blue light is off
 				x = True
+			else:
+				print('error')
 			#else: # used for error checking
 			#	print("error on " + data_arr["vacant"])
 		else: # this confirms out of order
-			GPIO.output(3, 0) # green light is off
+			GPIO.output(27, 0) # green light is off
 			GPIO.output(4, 0) # red light is off
 			GPIO.output(17,1) # blue light is on
 	# else: #the time is up, need to set a way to reset this!
 	# 	temp += 1
 	# 	print(temp)
 	# 	if temp > 10000: #turns the red light on to indicate time is up
-	# 		GPIO.output(3,0)
+	# 		GPIO.output(27,0)
 	# 		GPIO.output(4,1)
 	# 		GPIO.output(17,0)
-GPIO.cleanup()
+#GPIO.cleanup()
